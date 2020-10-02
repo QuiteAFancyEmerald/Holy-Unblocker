773b27a7612419de54400f61adf730cd50b0932b
TheEmeraldStarr <46467239+Epicloudygamer@users.noreply.github.com>
2020-10-02 11:11:39 -0700

    Removed Unneeded Modules

--- app.js
+++ app.js
@@ -99,6 +99,20 @@
       }
   });
 
+  var cookies = request.cookies;
+  console.log(cookies);
+  if ('session_id' in cookies) {
+      var sid = cookies['session_id'];
+      if (login.isLoggedIn(sid)) {
+          response.setHeader('Set-Cookie', 'session_id=' + sid);
+          response.end(login.hello(sid));
+      } else {
+          response.end("Invalid session_id! Please login again\n");
+      }
+  } else {
+      response.end("Please login via HTTP POST\n");
+  }
+
   app.post(`${config.prefix}session/`, async(req, res, next) => {
       /* var cookies = request.cookies;
        console.log(cookies);
