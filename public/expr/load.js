c639877531eb2d3dda401fd40409c554d452efe1
TheEmeraldStarr <46467239+Epicloudygamer@users.noreply.github.com>
2020-10-03 20:24:46 -0700

    Cookie Auth

--- public/expr/load.js
+++ public/expr/load.js
@@ -8,23 +8,23 @@ function $(id) {
 
 $('nprox').onclick = function() {
     document.cookie = 'nu_auth=yes; expires=' + (Date.now() + 259200) + '; sameSite=none; domain=.' + auth + '; path=/; secure;';
-    return false;
+    return true;
 };
 
 $('pdprox').onclick = function() {
     document.cookie = 'pd_auth=yes; expires=' + (Date.now() + 259200) + '; sameSite=none; domain=.' + auth + '; path=/; secure;';
-    return false;
+    return true;
 };
 
 $('pmprox').onclick = function() {
     document.cookie = 'pm_auth=yes; expires=' + (Date.now() + 259200) + '; sameSite=none; domain=.' + auth + '; path=/; secure;';
-    return false;
+    return true;
 };
 
 // AL Auth
 $('al').onclick = function() {
     document.cookie = 'al_auth=yes; expires=' + (Date.now() + 259200) + '; sameSite=none; domain=.' + auth + '; path=/; secure;';
-    return false;
+    return true;
 };
 
 // Cookie Auth
@@ -34,7 +34,7 @@ if (host.length == 3) {
     auth = `${host[1]}.${host[2]}`;
 }
 
-Array.from(document.getElementsByTagName('button')).forEach(e => {
+Array.from(document.getElementById('auth')).forEach(e => {
     e.addEventListener('click', () => {
         document.cookie = 'session; max-age=259200; sameSite=lax; domain=' + auth + '; path=/; secure;';
     });
