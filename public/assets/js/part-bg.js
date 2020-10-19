9bb187c4dc148acd2666e25285dc76641fadb93b
TheEmeraldStarr <theemeraldstarr@gmail.com>
2020-10-19 23:41:54 +0000

    Updated Styles

---
+++ public/assets/js/part-bg.js
@@ -0,0 +1,4 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+                        particlesJS.load('particles-js', 'particles.json', function() {
+                            console.log('particlesjs loaded.......');
+                        });
