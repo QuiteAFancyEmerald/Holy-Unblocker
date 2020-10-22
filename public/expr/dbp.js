aad8b90478407b775e9df236b83d5ca3c75ab6ad
TheEmeraldStarr <46467239+Epicloudygamer@users.noreply.github.com>
2020-10-22 11:12:14 -0700

    Updated Discord Page

---
+++ public/expr/dbp.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+    var url = `https://discord.com/login`;
+    var det = document.domain;
+    var domain = det.replace('www.', '').split(/[/?#]/)[0];
+    const origin = btoa(url)
+    window.location.href = "https://cdn." + domain + "/fetch/utils/?url=" + origin;
+    document.cookie = '__alloy_cookie_auth=yes; expires=' + (Date.now() + 259200) + '; SameSite=Lax; domain=.' + auth + '; path=/; Secure;';
+    return false;
+};
+
+// Cookie Auth
+var host = location.hostname.split('.');
+var auth = location.hostname;
+if (host.length == 3) {
+    auth = `${host[1]}.${host[2]}`;
+}
