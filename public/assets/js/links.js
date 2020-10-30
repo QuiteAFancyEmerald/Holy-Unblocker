910b351ce125fc884a0907a8f9b482458b05156b
QuiteAFancyEmerald <theemeraldstarr@gmail.com>
2020-10-30 20:15:27 +0000

    Updated Links

--- public/assets/js/links.js
+++ public/assets/js/links.js
@@ -2,15 +2,15 @@ function $(id) {
     return document.getElementById(id);
 };
 
-document.getElementById("hblink").href = "//github.com/QuiteAFancyEmerald/HolyUnblockerPublic";
+document.getElementById("hblink").href = "https://github.com/QuiteAFancyEmerald/HolyUnblockerPublic";
 
-document.getElementById("hblink2").href = "//github.com/QuiteAFancyEmerald/HolyUnblockerPublic";
+document.getElementById("hblink2").href = "https://github.com/QuiteAFancyEmerald/HolyUnblockerPublic";
 
-document.getElementById("allink").href = "//github.com/titaniumnetwork-dev/alloyproxy";
+document.getElementById("allink").href = "https://github.com/titaniumnetwork-dev/alloyproxy";
 
 document.getElementById("tnlink").href = "https://discord.com/invite/Dw6C7p5";
 
-document.getElementById("pmlink").href = "//github.com/vibedivide/powermouse";
+document.getElementById("pmlink").href = "https://github.com/vibedivide/powermouse";
 
 document.getElementById("qrlink").href = "https://support.discord.com/hc/en-us/articles/360039213771-QR-Code-Login-FAQ"
 
