2490da72aceb0ce2c5c910bec0b7f2a94483fd4a
TheEmeraldStarr <46467239+Epicloudygamer@users.noreply.github.com>
2020-11-01 12:53:59 -0800

    Edited Links

--- public/assets/js/links.js
+++ public/assets/js/links.js
@@ -8,12 +8,16 @@ document.getElementById("hblink2").href = "https://github.com/QuiteAFancyEmerald
 
 document.getElementById("allink").href = "https://github.com/titaniumnetwork-dev/alloyproxy";
 
+document.getElementById("plink").href = "https://github.com/vibedivide/powermouse";
+
 document.getElementById("tnlink").href = "https://discord.com/invite/Dw6C7p5";
 
 document.getElementById("pmlink").href = "https://github.com/vibedivide/powermouse";
 
-document.getElementById("qrlink").href = "https://support.discord.com/hc/en-us/articles/360039213771-QR-Code-Login-FAQ"
+document.getElementById("qrlink").href = "https://support.discord.com/hc/en-us/articles/360039213771-QR-Code-Login-FAQ";
 
-document.getElementById("nulink").href = "https://github.com/nfriedly/node-unblocker"
+document.getElementById("nulink").href = "https://github.com/nfriedly/node-unblocker";
 
-document.getElementById("pylink").href = "https://github.com/BinBashBanana/PyDodge"
+document.getElementById("pylink").href = "https://github.com/BinBashBanana/PyDodge";
+
+document.getElementById("nclink").href = "https://nodeclusters.com";
