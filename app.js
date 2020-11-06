9aa7f459e23fc781cc4e673e51e007f34a38cc4a
QuiteAFancyEmerald <theemeraldstarr@gmail.com>
2020-11-06 17:57:48 +0000

    Fixed some errors

--- app.js
+++ app.js
@@ -391,11 +391,20 @@ app.use(config.prefix, async (req, res, next) => {
     res.send(proxy.sendResponse);
 });
 
+app.post('/', async (req, res) => {
+    switch (req.url) {
+        case '/':
+            return res.send(fs.readFileSync(path.join(__dirname, 'public', 'index.html'), 'utf8'));
+    }
+});
+
 //Querystrings
-app.get('/', async (req, res) => {
+app.get('/', async (req, res, t) => res.send(fs.readFileSync(path.join(__dirname, 'public', 'pages,index.html,info.html,archive,archive,hidden.html'.split(',')['/,/?in,/?fg,/?rr,/?j'.split(',').indexOf(req.url) + 1], ',surf.html,f.html,run.html,frames,redirects3,proxnav5,nav7'.replace(/,[^,]+/g, e => ([] + e.match(/\D+/)).repeat(+e.match(/\d+/) + 1)).split(',')[t = 'z,fg,rr,k,dd,n,yh,ym,a,b,y,e,d,p,c,f,g,h,i,m,t,x'.split(',').indexOf(req.url.slice(2)) + 1], (t = ',,,,krunker,discordprox,chatbox,ythub,ytmobile,alloy,node,youtube,pydodge,discordhub,pmprox,credits,flash,gtools,games5,icons,gba,terms,bookmarklets'.split(',')[t]) && t + '.html'), 'utf8')));
+//Querystrings Old
+/* app.get('/', async (req, res) => {
 
 
-    /*  
+     
         const path = require("path"); //Use this for path.
         
         fs.readFileSync( path, options );
@@ -407,11 +416,9 @@ app.get('/', async (req, res) => {
              encoding: 'utf8'
            }));
         }
-    */
-
-    /*    var hbsites = {}; 
+       var hbsites = {}; 
         && hostname == hbsites
-    */
+
 
     switch (req.url) {
         case '/':
@@ -538,9 +545,9 @@ app.get('/', async (req, res) => {
     switch (req.url) {
         case '/?j':
             return res.send(fs.readFileSync(path.join(__dirname, 'public', 'hidden.html'), 'utf8'))
-    }
+    } 
 
-});
+}); */
 
 app.use('/', express.static('public'));
 
