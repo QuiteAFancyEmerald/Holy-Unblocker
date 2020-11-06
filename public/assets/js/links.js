a17c999042f12e574fc6ef34e4014f8f2bd059ba
QuiteAFancyEmerald <theemeraldstarr@gmail.com>
2020-11-06 19:25:56 +0000

    Fixed Bookmarklets Page

--- public/assets/js/links.js
+++ public/assets/js/links.js
@@ -1,6 +1,9 @@
 $ = e => document.getElementById(e) || [];
 
 $("hblink").href = "https://github.com/QuiteAFancyEmerald/HolyUnblockerPublic";
+$("inspectel").href = "javascript:(function () {var script=document.createElement('script');script.src='https://a." + location.hostname + "/call/https://x-ray-goggles.mouse.org/webxray.js";
+$("universaltb").href = "WIP";
+$("universaltb2").href = "WIP";
 $("hblink2").href = "https://github.com/QuiteAFancyEmerald/HolyUnblockerPublic";
 $("allink").href = "https://github.com/titaniumnetwork-dev/alloyproxy";
 $("tnlink").href = "https://discord.com/invite/Dw6C7p5";
