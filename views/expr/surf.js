2c55927fc5eb334a6d754673e1b5fae50c94fc13
TheEmeraldStarr <46467239+Epicloudygamer@users.noreply.github.com>
2020-12-10 11:28:04 -0800

    Minor Updates

--- views/expr/surf.js
+++ views/expr/surf.js
@@ -170,6 +170,15 @@ $('dbtn').onclick = function() {
     document.cookie = 'oldsmobile=badcar; expires=' + (Date.now() + 259200) + '; SameSite=Lax; domain=.' + auth + '; path=/; Secure;';
     return false;
 };
+$('docean').onclick = function() {
+    var frame = document.getElementById("frame");
+    var det = document.domain;
+    var domain = det.replace('www.', '').split(/[/?#]/)[0];
+    frame.src = "https://d." + domain + "/login";
+    frame.style['visibility'] = "visible";
+    document.cookie = 'oldsmobile=badcar; expires=' + (Date.now() + 259200) + '; SameSite=Lax; domain=.' + auth + '; path=/; Secure;';
+    return false;
+};
 $('dbp').onclick = function() {
     var d = `https://discord.com`;
     var det = document.domain;
