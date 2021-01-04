2474d048bb6b5ee527604192aae5ba62bf9fe40c
QuiteAFancyEmerald <theemeraldstarr@gmail.com>
2021-01-04 20:12:58 +0000

    Added Status Page

--- views/assets/js/links.js
+++ views/assets/js/links.js
@@ -6,7 +6,7 @@
 $ = e => document.getElementById(e) || [];
 
 $("hblink").href = "https://github.com/QuiteAFancyEmerald/HolyUnblockerPublic";
-$("inspectel").href = "javascript:(function () {var script=document.createElement('script');script.src='https://a." + location.hostname + "/call/https://x-ray-goggles.mouse.org/webxray.js";
+$("inspectel").href = "javascript:(function () {var script=document.createElement('script');script.src='https://cdn." + location.hostname + "/https://x-ray-goggles.mouse.org/webxray.js";
 $("universaltb").href = "WIP";
 $("universaltb2").href = "WIP";
 $("hblink2").href = "https://github.com/QuiteAFancyEmerald/HolyUnblockerPublic";
@@ -16,6 +16,7 @@ $("plink").href = "https://github.com/vibedivide/powermouse";
 $("nclink").href = "https://nodeclusters.com";
 $("qrlink").href = "https://support.discord.com/hc/en-us/articles/360039213771-QR-Code-Login-FAQ";
 $("nulink").href = "https://github.com/nfriedly/node-unblocker";
-$("pylink").href = "https://github.com/BinBashBanana/PyDodge"
-
-// Add game page links here
+$("pylink").href = "https://github.com/BinBashBanana/PyDodge";
+$("temp1").href = "https://depressed.gq";
+$("temp2").href = "https://constant.cf";
+$("temp3").href = "https://coolseagull.design"
