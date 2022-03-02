e206734953e77bf3c0e1b56219d868d4915939df
Quite A Fancy Emerald <46467239+QuiteAFancyEmerald@users.noreply.github.com>
2022-03-01 22:07:01 -0800

    Fixed UV (mistake)

--- views/uv.config.js
+++ views/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://cdn.' + location.hostname + '/bare/',
+    bare: '/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv.handler.js',
