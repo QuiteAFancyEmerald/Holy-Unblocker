3c7652b41db33bd07e913bd65862e6424e067cb2
Entrpix <korr626@outlook.com>
2024-07-06 19:20:46 -0400

    wisp

---
+++ views/uv/sw.js
@@ -0,0 +1,18 @@
+
+importScripts('/epoxy/index.js');
+importScripts('/uv/uv.bundle.js');
+importScripts('/uv/uv.config.js');
+importScripts(__uv$config.sw || '/uv/uv.sw.js');
+
+const uv = new UVServiceWorker();
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        (async ()=>{
+            if(uv.route(event)) {
+                return await uv.fetch(event);
+            }
+            return await fetch(event.request);
+        })()
+    );
+});
