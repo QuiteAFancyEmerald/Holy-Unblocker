a5e3a6255b574d7bc3ed687159dc1787b0ab9560
Entrpix <korr626@outlook.com>
2024-07-07 18:06:25 -0400

    speed is better. fixed proxy yay

--- views/assets/js/register-sw.js
+++ views/assets/js/register-sw.js
@@ -1,20 +1,16 @@
-const stockSW = "/uv/sw.js";
-const swAllowedHostnames = ["localhost", "127.0.0.1"];
-
-
 async function registerSW() {
-  if (!navigator.serviceWorker) {
-    if (
-      location.protocol !== "https:" &&
-      !swAllowedHostnames.includes(location.hostname)
-    )
-      throw new Error("Service workers cannot be registered without https.");
-
-    throw new Error("Your browser doesn't support service workers.");
+    if (!navigator.serviceWorker) {
+      if (
+        location.protocol !== "https:" &&
+        !swAllowedHostnames.includes(location.hostname)
+      )
+        throw new Error("Service workers cannot be registered without https.");
+  
+      throw new Error("Your browser doesn't support service workers.");
+    }
+  
+    await navigator.serviceWorker.register(stockSW);
+  
+    let wispUrl = (location.protocol === "https:" ? "wss" : "ws") + "://" + location.host + "/wisp/";
+    await BareMux.SetTransport("EpxMod.EpoxyClient", { wisp: wispUrl });
   }
-
-  await navigator.serviceWorker.register(stockSW);
-
-  let wispUrl = (location.protocol === "https:" ? "wss" : "ws") + "://" + location.host + "/wisp/";
-  await BareMux.SetTransport("EpxMod.EpoxyClient", { wisp: wispUrl });
-}
