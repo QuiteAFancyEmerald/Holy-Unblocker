2b4050ee81f88d4ec05092617d34326f8ce57e8b
00Fjongl <65314359+00Fjongl@users.noreply.github.com>
2024-07-08 12:20:52 -0500

    Swap back to ES6

--- views/assets/js/card.js
+++ views/assets/js/card.js
@@ -7,20 +7,20 @@
 // Function declarations
 //  Track the cursor position with respect to the top left of the card.
 //  The "this" keyword gets the element that invoked the event listener.
-function handleMouseMove(e) {
+const handleMouseMove = e => {
   const rect = this.getBoundingClientRect();
   const x = e.clientX - rect.left;
   const y = e.clientY - rect.top;
 
   this.style.setProperty("--mouse-x", `${x}px`);
   this.style.setProperty("--mouse-y", `${y}px`);
-}
+},
 
 //  Reset the cursor tracking variables when the cursor leaves the card.
-function handleMouseLeave() {
+handleMouseLeave = () => {
   this.style.setProperty("--mouse-x", `50%`);
   this.style.setProperty("--mouse-y", `50%`);
-}
+};
 
 // Query and add event listeners
 const shimmerEffects = document.querySelectorAll(".box-card");
