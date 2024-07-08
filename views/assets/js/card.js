9e177992a4f5de7a368baf3a927d2c6342a68f52
QuiteAFancyEmerald <46467239+QuiteAFancyEmerald@users.noreply.github.com>
2024-07-08 09:53:53 -0700

    Fixed card shimmer

--- views/assets/js/card.js
+++ views/assets/js/card.js
@@ -4,6 +4,25 @@
 /* Card Shimmer Mouse Follow Script
 /* ----------------------------------------------- */
 
+// Function declarations
+//  Track the cursor position with respect to the top left of the card.
+//  The "this" keyword gets the element that invoked the event listener.
+function handleMouseMove(e) {
+  const rect = this.getBoundingClientRect();
+  const x = e.clientX - rect.left;
+  const y = e.clientY - rect.top;
+
+  this.style.setProperty("--mouse-x", `${x}px`);
+  this.style.setProperty("--mouse-y", `${y}px`);
+}
+
+//  Reset the cursor tracking variables when the cursor leaves the card.
+function handleMouseLeave() {
+  this.style.setProperty("--mouse-x", `50%`);
+  this.style.setProperty("--mouse-y", `50%`);
+}
+
+// Query and add event listeners
 const shimmerEffects = document.querySelectorAll(".box-card");
 
 //  Attach CSS variables, mouse-x and mouse-y, to elements that will be
@@ -13,20 +32,3 @@ shimmerEffects.forEach(shimmerEffect => {
   shimmerEffect.addEventListener("mousemove", handleMouseMove);
   shimmerEffect.addEventListener("mouseleave", handleMouseLeave);
 });
-
-//  Track the cursor position with respect to the top left of the card.
-//  The "this" keyword gets the element that invoked the event listener.
-const handleMouseMove = e => {
-  const rect = this.getBoundingClientRect();
-  const x = e.clientX - rect.left;
-  const y = e.clientY - rect.top;
-
-  this.style.setProperty("--mouse-x", `${x}px`);
-  this.style.setProperty("--mouse-y", `${y}px`);
-};
-
-//  Reset the cursor tracking variables when the cursor leaves the card.
-const handleMouseLeave = () => {
-  this.style.setProperty("--mouse-x", `50%`);
-  this.style.setProperty("--mouse-y", `50%`);
-};
