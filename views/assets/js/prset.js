3f10060708f59dfefc05be89d298071b05e11a76
QuiteAFancyEmerald <46467239+QuiteAFancyEmerald@users.noreply.github.com>
2024-07-09 18:44:08 -0700

    updated syntax to support refactored common file

--- views/assets/js/prset.js
+++ views/assets/js/prset.js
@@ -1,23 +1,29 @@
 /* -----------------------------------------------
-/* Authors: OlyB
+/* Authors: OlyB, QuiteAFancyEmerald
 /* GNU Affero General Public License v3.0: https://www.gnu.org/licenses/agpl-3.0.en.html
 /* prSet script
 /* ----------------------------------------------- */
 
 function prSet(type) {
-    var prUrl = document.getElementById("pr-url"),
-    prGo1 = document.getElementById("pr-go1"),
-    prGo2 = document.getElementById("pr-go2");
+    var prUrl = document.getElementById("pr-url");
+    var prGo1 = document.getElementById("pr-go1");
+    var prGo2 = document.getElementById("pr-go2");
 
     prUrl.addEventListener("keydown", function(e) {
-        if (e.code == "Enter" && prUrl.value) goProx[type](prUrl.value);
-    }, false);
+        if (e.code === "Enter" && prUrl.value.trim() !== "") {
+            goProx[type](prUrl.value.trim());
+        }
+    });
 
     prGo1.addEventListener("click", function() {
-        if (prUrl.value) goProx[type](prUrl.value);
-    }, false);
+        if (prUrl.value.trim() !== "") {
+            goProx[type](prUrl.value.trim(), "window"); // Classic Mode
+        }
+    });
 
     prGo2.addEventListener("click", function() {
-        if (prUrl.value) goProx[type](prUrl.value, true);
-    }, false);
+        if (prUrl.value.trim() !== "") {
+            goProx[type](prUrl.value.trim(), "stealth"); // Stealth Mode
+        }
+    });
 }
