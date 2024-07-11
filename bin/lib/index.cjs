f92ba67e72cd67f2d94574f282ee5920832d19fc
Percs <83934299+Percslol@users.noreply.github.com>
2024-07-11 10:35:39 -0500

    update to uv v2

--- bin/lib/index.cjs
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const { resolve } = require('node:path');
-
-const uvPath = resolve(__dirname, '..', 'dist');
-
-exports.uvPath = uvPath;
--- views/uv/sw.js
+++ views/uv/sw.js
@@ -1,4 +1,3 @@
-importScripts("../epoxy/index.js");
 importScripts("uv.bundle.js");
 importScripts("uv.config.js");
 importScripts(__uv$config.sw || "uv.sw.js");
@@ -8,7 +7,7 @@ const uv = new UVServiceWorker();
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     (async () => {
-      if (event.request.url.startsWith(location.origin + __uv$config.prefix)) {
+      if (uv.route(event)) {
         return await uv.fetch(event);
       }
       return await fetch(event.request);
