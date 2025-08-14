c328e3ca8c67caae388d8dea030289036a94f004
QuiteAFancyEmerald <46467239+QuiteAFancyEmerald@users.noreply.github.com>
2025-08-13 23:17:03 -0700

    update: implemented wip light theme

--- src/routes.mjs
+++ src/routes.mjs
@@ -225,7 +225,8 @@ const cookingInserts = insert.content,
   splashRandom = insert.splash,
   VersionValue = insert.version,
   cacheBustList = {
-    'styles.css': 'styles-1735118314.css',
+    'styles.css': 'styles-1755147161.css',
+    'styles-light.css': 'styles-light-1755147161.css',
     'common.js': 'common-1735118314.js',
   };
 
