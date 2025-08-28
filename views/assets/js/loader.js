cffdf823ca28aa604d4a34b8ca369a569b05a63e
00Fjongl <65314359+00Fjongl@users.noreply.github.com>
2025-08-28 02:08:55 -0500

    refactor: improve page request mechanism

--- views/assets/js/loader.js
+++ views/assets/js/loader.js
@@ -22,148 +22,178 @@
   };
   if (localStorage.getItem('{{hu-lts}}-loader-key') !== navigator.userAgent)
     return displayErrorPage();
-  const loadPage = () => {
-    removeEventListener('load', loadPage);
-    fetch(location.pathname + '.ico', { mode: 'same-origin' })
-      .then((response) => {
-        response.blob().then((blob) => {
-          new Response(
-            blob.stream().pipeThrough(new DecompressionStream('gzip'))
-          )
-            .text()
-            .then((text) => {
-              ((currentDoc, newDoc) => {
-                const deferScripts = [],
-                  syncScripts = [];
-                let reachedEnd = false,
-                  waitForHead = false,
-                  headScripts = 0;
-                const bodyLoader = () => {
-                  headScripts--;
-                  if (waitForHead && headScripts <= 0) {
-                    waitForHead = false;
-                    currentDoc.body.replaceWith(recursiveClone(newDoc.body));
-                    if (reachedEnd) loadNextScript(false)();
-                  }
-                  return waitForHead;
-                };
-                const loadNextScript = (isDefer, currentScript) => () => {
-                  if (
-                    !isDefer &&
-                    currentScript &&
-                    'head' === currentScript.parentElement.tagName.toLowerCase()
-                  )
-                    bodyLoader();
-                  let nextScript = [...currentDoc.scripts].find(
-                    (script) =>
-                      script.getAttribute('itemprop') === 'script-insert' &&
-                      script.defer === isDefer
-                  );
-                  if (nextScript) {
-                    const replacement = isDefer
-                      ? deferScripts.shift()
-                      : syncScripts.shift();
-                    nextScript.replaceWith(replacement);
-                    if (replacement.childNodes.length > 0)
-                      loadNextScript(isDefer, replacement)();
-                  } else if (!isDefer && !waitForHead) loadNextScript(true)();
-                  else reachedEnd = true;
-                };
-                const recursiveClone = (node) => {
-                  if (node.nodeType !== Node.ELEMENT_NODE) return node;
-                  const nodeName = node.tagName.toLowerCase();
-                  if (['svg', 'xml'].includes(nodeName))
-                    return node.cloneNode(1);
-                  let elementCopy = currentDoc.createElement(nodeName);
-                  let j = 0,
-                    nodeList = [...node.attributes];
-                  for (; j < nodeList.length; j++)
-                    elementCopy.setAttribute(
-                      nodeList[j].nodeName,
-                      nodeList[j].nodeValue || ''
-                    );
-                  nodeList = [...node.childNodes];
-                  for (j = 0; j < nodeList.length; j++)
-                    elementCopy.appendChild(recursiveClone(nodeList[j]));
-                  if ('script' === nodeName && !node.async) {
-                    const isDefer =
-                      node.defer || 'module' === node.type.toLowerCase();
-                    let replacement = currentDoc.createElement('script');
-                    if (isDefer) replacement.setAttribute('defer', '');
-                    replacement.setAttribute('itemprop', 'script-insert');
-                    if (node.childNodes.length <= 0) {
-                      elementCopy.addEventListener(
-                        'load',
-                        loadNextScript(isDefer, elementCopy)
-                      );
-                      elementCopy.addEventListener(
-                        'error',
-                        loadNextScript(isDefer, elementCopy)
-                      );
+  const loadPage =
+    (destination = location, pushState = true) =>
+    () => {
+      removeEventListener('load', loadPage);
+      fetch(destination.pathname + '.ico', { mode: 'same-origin' })
+        .then((response) => {
+          if (destination !== location && pushState) {
+            if (response.status === 200) history.pushState({}, '', destination);
+            else return location.assign(new URL(destination, location));
+          }
+          response.blob().then((blob) => {
+            new Response(
+              blob.stream().pipeThrough(new DecompressionStream('gzip'))
+            )
+              .text()
+              .then((text) => {
+                ((currentDoc, newDoc) => {
+                  const deferScripts = [],
+                    syncScripts = [];
+                  let reachedEnd = false,
+                    waitForHead = false,
+                    headScripts = 0;
+                  const bodyLoader = () => {
+                    headScripts--;
+                    if (waitForHead && headScripts <= 0) {
+                      waitForHead = false;
+                      currentDoc.body.replaceWith(recursiveClone(newDoc.body));
+                      if (reachedEnd) loadNextScript(false)();
                     }
-                    if (isDefer) deferScripts.push(elementCopy);
-                    else {
-                      syncScripts.push(elementCopy);
-                      if ('head' === node.parentElement.tagName.toLowerCase())
-                        headScripts++;
-                    }
-                    return replacement;
-                  } else if (['style', 'link'].includes(nodeName)) {
-                    if ('link' === nodeName && !/^stylesheet$/i.test(node.rel))
-                      return elementCopy;
-                    else if (node.childNodes.length <= 0) {
-                      elementCopy.addEventListener('load', bodyLoader);
-                      elementCopy.addEventListener('error', bodyLoader);
-                      if ('head' === node.parentElement.tagName.toLowerCase())
-                        headScripts++;
-                    }
-                  }
-                  return elementCopy;
-                };
-                let currentType = currentDoc.doctype,
-                  newType = newDoc.doctype,
-                  currentDocNode = currentDoc.documentElement,
-                  newDocNode = newDoc.documentElement;
-                if (currentType)
-                  if (newType) currentType.replaceWith(newType);
-                  else currentType.remove();
-                else if (newType) currentDoc.prepend(newType);
-                if (currentDocNode)
-                  if (newDocNode) {
+                    return waitForHead;
+                  };
+                  const loadNextScript = (isDefer, currentScript) => () => {
                     if (
-                      currentDocNode.tagName === newDocNode.tagName &&
-                      currentDoc.head &&
-                      newDoc.head &&
-                      currentDoc.body &&
-                      newDoc.body
-                    ) {
-                      [...currentDocNode.attributes].forEach((attribute) => {
-                        currentDocNode.removeAttribute(attribute.nodeName);
-                      });
-                      [...newDocNode.attributes].forEach((attribute) => {
-                        currentDocNode.setAttribute(
-                          attribute.nodeName,
-                          attribute.nodeValue || ''
+                      !isDefer &&
+                      currentScript &&
+                      'head' ===
+                        currentScript.parentElement.tagName.toLowerCase()
+                    )
+                      bodyLoader();
+                    let nextScript = [...currentDoc.scripts].find(
+                      (script) =>
+                        script.getAttribute('itemprop') === 'script-insert' &&
+                        script.defer === isDefer
+                    );
+                    if (nextScript) {
+                      const replacement = isDefer
+                        ? deferScripts.shift()
+                        : syncScripts.shift();
+                      nextScript.replaceWith(replacement);
+                      if (replacement.childNodes.length > 0)
+                        loadNextScript(isDefer, replacement)();
+                    } else if (!isDefer && !waitForHead) loadNextScript(true)();
+                    else reachedEnd = true;
+                  };
+                  const recursiveClone = (node) => {
+                    if (node.nodeType !== Node.ELEMENT_NODE) return node;
+                    const nodeName = node.tagName.toLowerCase();
+                    if (['svg', 'xml'].includes(nodeName))
+                      return node.cloneNode(1);
+                    let elementCopy = currentDoc.createElement(nodeName);
+                    let j = 0,
+                      nodeList = [...node.attributes];
+                    for (; j < nodeList.length; j++) {
+                      let attrName = nodeList[j].nodeName;
+                      let attrValue = nodeList[j].nodeValue;
+                      elementCopy.setAttribute(attrName, attrValue || '');
+                      if (attrName.toLowerCase() === 'href')
+                        try {
+                          new URL(attrValue);
+                        } catch (e) {
+                          if (attrValue[0] === '.' || attrValue[0] === '/')
+                            elementCopy.addEventListener('click', (event) => {
+                              event.preventDefault();
+                              if (attrValue === '{{route}}{{/}}')
+                                attrValue = '{{route}}{{/index}}';
+                              loadPage(new URL(attrValue, location))();
+                            });
+                        }
+                    }
+                    nodeList = [...node.childNodes];
+                    for (j = 0; j < nodeList.length; j++)
+                      elementCopy.appendChild(recursiveClone(nodeList[j]));
+                    if ('script' === nodeName && !node.async) {
+                      const isDefer =
+                        node.defer || 'module' === node.type.toLowerCase();
+                      let replacement = currentDoc.createElement('script');
+                      if (isDefer) replacement.setAttribute('defer', '');
+                      replacement.setAttribute('itemprop', 'script-insert');
+                      if (node.childNodes.length <= 0) {
+                        elementCopy.addEventListener(
+                          'load',
+                          loadNextScript(isDefer, elementCopy)
                         );
-                      });
-                      waitForHead = true;
-                      currentDoc.head.replaceWith(recursiveClone(newDoc.head));
-                    } else
-                      currentDocNode.replaceWith(recursiveClone(newDocNode));
-                  } else currentDocNode.remove();
-                else if (newDocNode)
-                  currentDocNode.appendChild(recursiveClone(newDocNode));
+                        elementCopy.addEventListener(
+                          'error',
+                          loadNextScript(isDefer, elementCopy)
+                        );
+                      }
+                      if (isDefer) deferScripts.push(elementCopy);
+                      else {
+                        syncScripts.push(elementCopy);
+                        if ('head' === node.parentElement.tagName.toLowerCase())
+                          headScripts++;
+                      }
+                      return replacement;
+                    } else if (['style', 'link'].includes(nodeName)) {
+                      if (
+                        'link' === nodeName &&
+                        !/^stylesheet$/i.test(node.rel)
+                      )
+                        return elementCopy;
+                      else if (node.childNodes.length <= 0) {
+                        elementCopy.addEventListener('load', bodyLoader);
+                        elementCopy.addEventListener('error', bodyLoader);
+                        if ('head' === node.parentElement.tagName.toLowerCase())
+                          headScripts++;
+                      }
+                    }
+                    return elementCopy;
+                  };
+                  let currentType = currentDoc.doctype,
+                    newType = newDoc.doctype,
+                    currentDocNode = currentDoc.documentElement,
+                    newDocNode = newDoc.documentElement;
+                  if (currentType)
+                    if (newType) currentType.replaceWith(newType);
+                    else currentType.remove();
+                  else if (newType) currentDoc.prepend(newType);
+                  if (currentDocNode)
+                    if (newDocNode) {
+                      if (
+                        currentDocNode.tagName === newDocNode.tagName &&
+                        currentDoc.head &&
+                        newDoc.head &&
+                        currentDoc.body &&
+                        newDoc.body
+                      ) {
+                        [...currentDocNode.attributes].forEach((attribute) => {
+                          currentDocNode.removeAttribute(attribute.nodeName);
+                        });
+                        [...newDocNode.attributes].forEach((attribute) => {
+                          currentDocNode.setAttribute(
+                            attribute.nodeName,
+                            attribute.nodeValue || ''
+                          );
+                        });
+                        waitForHead = true;
+                        currentDoc.head.replaceWith(
+                          recursiveClone(newDoc.head)
+                        );
+                      } else
+                        currentDocNode.replaceWith(recursiveClone(newDocNode));
+                    } else currentDocNode.remove();
+                  else if (newDocNode)
+                    currentDocNode.appendChild(recursiveClone(newDocNode));
 
-                loadNextScript(false)();
-              })(document, new DOMParser().parseFromString(text, 'text/html'));
-            });
+                  loadNextScript(false)();
+                })(
+                  document,
+                  new DOMParser().parseFromString(text, 'text/html')
+                );
+              });
+          });
+        })
+        .catch((error) => {
+          console.log(error);
+          displayErrorPage();
         });
-      })
-      .catch((error) => {
-        console.log(error);
-        displayErrorPage();
-      });
-  };
-  if (document.readyState === 'complete') loadPage();
-  else addEventListener('load', loadPage);
+    };
+  if (document.readyState === 'complete') loadPage()();
+  else addEventListener('load', loadPage());
+  addEventListener('popstate', () => {
+    loadPage(location, false)();
+  });
 })();
